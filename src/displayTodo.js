785e2fe4755e826a29252ae81de76bd5f24de1f2
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-04 16:21:51 +0500

    Add event listener to mark task as complete

--- src/displayTodo.js
+++ src/displayTodo.js
@@ -1,14 +1,27 @@
 import { arrayTodo } from "."
+import { changeCompleteStatus } from "./changeCompleteStatus";
 function displayTodo()
 {
     const mainContent=document.querySelector(".main");
     mainContent.textContent="";
+    let i=0;
     arrayTodo.forEach(item => {
         const div=document.createElement("div");
         div.classList.add("items");
+        div.setAttribute("id",i);
+        i++;
         mainContent.appendChild(div);
         for(let key in item)
         {
+            if(key==="complete")
+            {
+                if(item[key]===true)
+                {
+                    div.style.color="green";
+                    div.classList.add("complete");
+                }
+                continue;
+            }
             const div2=document.createElement("div");
             div2.classList.add(key);
             div2.textContent=item[key];
@@ -16,5 +29,13 @@ function displayTodo()
 
         }
     })
+
+    const items=document.querySelectorAll(".items");
+    items.forEach(item =>
+    {
+        item.addEventListener("click",changeCompleteStatus);
+    });
+
 }
+
 export {displayTodo}
