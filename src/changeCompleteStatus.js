a081e5e59bff51e5acd8a378ccc5cade34de243d
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-04 18:28:42 +0500

    Add function for getting last character

--- src/changeCompleteStatus.js
+++ src/changeCompleteStatus.js
@@ -4,14 +4,17 @@ import { displayTodo } from "./displayTodo";
 function changeCompleteStatus(event)
 {
 
-    let lastChar=event.target.id;
-    lastChar=lastChar.charAt(lastChar.length-1)
+    let checkButtonID=getLastCharacter(event.target.id);
     
-    console.log(lastChar+"hola");
     console.log(event.target);
     console.log(event.target.id);
-    arrayTodo[lastChar].complete=!(arrayTodo[lastChar].complete);
-    console.log(arrayTodo[lastChar].complete);
+
+    arrayTodo[checkButtonID].complete=!(arrayTodo[checkButtonID].complete);
     displayTodo();
+}
+
+function getLastCharacter(string)
+{
+    return string.charAt(string.length-1);
 }
 export{changeCompleteStatus}
