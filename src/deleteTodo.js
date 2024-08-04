2c75366637645360bb7059173a5a69b60df0f2c8
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-04 19:49:06 +0500

    Implement delete todo feature

---
+++ src/deleteTodo.js
@@ -0,0 +1,13 @@
+import { arrayTodo } from ".";
+import { getLastCharacter } from "./changeCompleteStatus"
+import { displayTodo } from "./displayTodo";
+
+
+function deleteTodo(event)
+{
+    let deleteButtonID=getLastCharacter(event.target.id);
+    arrayTodo.splice(deleteButtonID,1);
+    displayTodo();    
+}
+
+export {deleteTodo}
