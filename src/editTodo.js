e31a648db0eb1895556f9c214bd701205860e77b
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-05 15:11:16 +0500

    Priority not being edited

--- src/editTodo.js
+++ src/editTodo.js
@@ -3,36 +3,73 @@ import { getLastCharacter } from "./changeCompleteStatus";
 import { displayTodo } from "./displayTodo";
 import { closeDialog, resetDialog, showDialog } from "./handleDialog";
 
-function editTodo(event) {
+function editTodo(event) 
+{
     console.log("edit");
     console.log(event.target.parentNode.id);
     console.log(event.target.textContent);
     console.log(event.target.classList.value);
-
+    
+    
+    
+    
     let temp = event.target.textContent;
+    let key=(event.target.classList.value);
+
+
     const userInput = document.createElement("input");
-    userInput.setAttribute("type", "text");
+    
+    if(key==="priority")
+    {
+        editPriority(event);
+        return 0;
+    }
+    
+    else if (key==="dueDate")
+        userInput.setAttribute("type", "date");
+
+    else
+        userInput.setAttribute("type", "text");
+
     userInput.value = temp;
 
-    // Clear the existing text content
     event.target.textContent = '';
 
-    // Append the input element to the target
     event.target.appendChild(userInput);
 
-    // Focus the input element for immediate editing
     userInput.focus();
 
-    // Optionally, handle input blur to save the changes
-    userInput.addEventListener("blur", () => {
-        // Update the target's text content with the input value
-        // event.target.textContent = userInput.value;
-        let key=(event.target.classList.value);
+    userInput.addEventListener("blur", () => 
+    {
         console.log("here "+arrayTodo[event.target.parentNode.id].key)
         arrayTodo[event.target.parentNode.id][key]=userInput.value;
         displayTodo();
         
     });
 }
+function editPriority(event) 
+{
+
+    const selection = document.createElement("select");
+    const option1 = document.createElement("option");
+    const option2 = document.createElement("option");
+
+    option1.textContent = "Normal";
+    option1.value = "Normal";
+    option2.textContent = "Urgent";
+    option2.value = "Urgent";
+
+    selection.appendChild(option1);
+    selection.appendChild(option2);
+
+    // Append the select element to the target
+    event.target.appendChild(selection);
+    selection.focus();
+
+    selection.addEventListener("change", e => {
+        event.target.textContent=e.target.value;
+    })
+}
+
 
 export{editTodo}
