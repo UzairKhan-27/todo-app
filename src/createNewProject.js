35a2f0b44f49162a5bef96b396b68333123afbb0
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-05 23:55:47 +0500

    call update dropdown

--- src/createNewProject.js
+++ src/createNewProject.js
@@ -1,19 +1,16 @@
 import { arrayProjects } from ".";
+import { updateProjectDropdown } from "./updateProjectDropdown";
 
 function createNewProject(event)
 {
     let user=prompt("val");
     arrayProjects.push(user);
     const li=document.createElement("li");
-    const plus=document.createElement("span");
-    plus.textContent="\t\t\t+";
-    // plus.style.display="inline";
     const list= document.querySelector(".lists")
     li.setAttribute("id",`list${arrayProjects.length-1}`)
     li.textContent=user;
     list.appendChild(li);
-    li.appendChild(plus);
-    // list.appendChild(plus);
     console.log(arrayProjects);
+    updateProjectDropdown();
 }
 export{createNewProject}
