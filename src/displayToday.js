0c01bfcdbe52a15a6a3ab59e9220881f652cae2f
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-05 18:40:06 +0500

    Add function to get todays date in string

--- src/displayToday.js
+++ src/displayToday.js
@@ -9,17 +9,20 @@ function displayToday()
 }
 function checkDueDateStatus(todo)
 {
-    const date = new Date();
 
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-day = day < 10 ? '0' + day : day;
-month = month < 10 ? '0' + month : month;
-// This arrangement can be altered based on how we want the date's format to appear.
-let currentDate = `${year}-${month}-${day}`;
-console.log(currentDate); // "17-6-2022"
+    let currentDate=getTodayDate();
+    console.log(currentDate);
 
     return todo.dueDate===currentDate;
+}
+function getTodayDate()
+{
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    day = day < 10 ? '0' + day : day;
+    month = month < 10 ? '0' + month : month;
+    return `${year}-${month}-${day}`;
 }
 export {displayToday}
